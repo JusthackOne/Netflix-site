e1fc37519d193d4af92b2f78a5ad93ac25cd7c9f
Михаил Недиков <nedikov.misha@yandex.ru>
2024-09-04 18:48:21 +0300

    [fix]: add vercel.json and fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "/api/:path*"
+      },
+      {
+        source: "/:path*",
+        destination: "/"
+      }
+    ];
+  }
 };
 
 export default nextConfig;
